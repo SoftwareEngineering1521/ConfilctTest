3a5efce73ea063dde8971e606807d2a6c78a4b00
Lzr <1581501440@qq.com>
2017-04-17 14:13:10 +0800

    测试用cpp

---
+++ 空心正三角形.cpp
@@ -0,0 +1,28 @@
+#include<iostream> 
+using namespace std;
+int main()
+{
+	int n;
+	while(cin>>n)
+{
+		for (int i=1;i<=n;i++)
+	{
+		for (int j=1;j<=2*n;j++)
+		{
+			if (i==n&&j<n+1)
+			cout<<"* ";
+			if (i==n&&j>=n+1)
+			break;
+			if (i<n)
+            {
+			if (j==n-i+1||j==n+i-1)
+			cout<<"*";
+			if (j!=n-i+1&&j!=n+i-1)
+			cout<<" ";
+		    }  
+		}
+		cout<<endl;
+	}     
+}
+    return 0;
+}
